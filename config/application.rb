67411cdfd474db55b015dafb7258f53d61022fe2
Jason Woodlee <woodleeb@amazon.com>
2023-12-12 22:30:09 -0500

    envs

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Demo
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << /.*\.awsapprunner\.com/
   end
 end
