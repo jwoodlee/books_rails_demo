9d97c28bc173d3d19459add59f1f215bd1555b64
Jason Woodlee <woodleeb@amazon.com>
2023-12-18 23:12:52 -0500

    envs

--- runme.sh
+++ runme.sh
@@ -8,6 +8,7 @@ export RAILS_ENV="production"
 env
 rails -v
 ruby -v
+bundle exec rails runner 'puts ActiveRecord::Base.configurations.inspect'
 bundle exec rails db:create db:migrate
 bundle exec rails s -b 0.0.0.0 -p 8080
 
